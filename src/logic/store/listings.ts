abb3b2880fd8ea59d18a9e27749c2d50e3b89177
samuelili <samuelli4521@gmail.com>
2025-01-02 14:55:50 -0800

    added createListing method

--- src/logic/store/listings.ts
+++ src/logic/store/listings.ts
@@ -1,16 +1,33 @@
-import {ensureAppInitialized} from "@/logic/firebaseApp.ts";
-import {collection, getFirestore} from "firebase/firestore";
+import { ensureAppInitialized } from '@/logic/firebaseApp.ts';
+import { addDoc, collection, getDoc, getFirestore } from 'firebase/firestore';
 
 ensureAppInitialized();
 
 const db = getFirestore();
 
-const listingsRef = collection(db, "listings");
+const listingsRef = collection(db, 'listings');
 
 type Listing = {
-  
+  uid: string;
+  title: string;
+  user: string;
+  pod: string;
+  location: string;
+  price: number;
+  description: string;
+  images: string[];
 };
 
-export async function createListing() {
+export type NewListing = Omit<Listing, 'uid'>;
 
+export async function createListing(listing: NewListing) {
+  const docRef = await addDoc(listingsRef, listing);
+  const snap = await getDoc(docRef);
+
+  if (snap.exists())
+    return {
+      uid: snap.id,
+      ...snap.data(),
+    } as Listing;
+  return null;
 }
